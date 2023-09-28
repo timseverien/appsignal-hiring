84c2058cd71af93e929db63b1df8e3b17166751b
Tim Severien <timseverien@gmail.com>
2023-09-28 15:22:20 +0200

    Add prototype for MetadataDistributionBar

--- components/MetadataDistributions.jsx
+++ components/MetadataDistributions.jsx
@@ -7,6 +7,32 @@ function MetadataDistributionAttribute({ distribution }) {
       <h3>
         {distribution.name} ({distribution.unique})
       </h3>
+      <MetadataDistributionBar data={[]} />
+    </div>
+  );
+}
+
+function MetadataDistributionBar({ data }) {
+  return (
+    <div className="flex basis-0 gap-px rounded-md overflow-hidden">
+      <button
+        className="bg-blue-100 overflow-hidden h-2"
+        style={{ flexGrow: 1 }}
+      >
+        <span class="sr-only">Foo</span>
+      </button>
+      <button
+        className="bg-blue-100 overflow-hidden h-2"
+        style={{ flexGrow: 2 }}
+      >
+        <span class="sr-only">Foo</span>
+      </button>
+      <button
+        className="bg-blue-100 overflow-hidden h-2"
+        style={{ flexGrow: 3 }}
+      >
+        <span class="sr-only">Foo</span>
+      </button>
     </div>
   );
 }
