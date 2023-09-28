11737976b85132dd206adccb893e2968e8e5ca76
Tim Severien <timseverien@gmail.com>
2023-09-28 20:05:54 +0200

    Add some unit tests for MetadataDistributions

--- components/MetadataDistributions.test.jsx
+++ components/MetadataDistributions.test.jsx
@@ -19,9 +19,96 @@ describe("MetadataDistributions", () => {
     expect(screen.getByText("Metadata distributions")).toBeInTheDocument();
   });
 
-  it("renders a metadata distribution category", () => {
+  it("renders headings for each metadata distribution attributes", () => {
     build();
 
     expect(screen.getByText("hostname (10)")).toBeInTheDocument();
+    expect(screen.getByText("revision (20)")).toBeInTheDocument();
+  });
+
+  it("renders a bar with distribution items", () => {
+    build({
+      distributions: [
+        {
+          name: "hostname",
+          total: 200,
+          unique: 10,
+          distributions: [
+            {
+              key: "distribution1",
+              value: 20,
+            },
+            {
+              key: "distribution2",
+              value: 20,
+            },
+          ],
+        },
+      ],
+    });
+
+    expect(screen.getByText("distribution1")).toBeInTheDocument();
+    expect(screen.getByText("distribution1")).toBeInTheDocument();
+  });
+
+  it("renders a bar with 'other' distribution item when number of item exceeds 10", () => {
+    build({
+      distributions: [
+        {
+          name: "hostname",
+          total: 220,
+          unique: 11,
+          distributions: [
+            {
+              key: "distribution1",
+              value: 20,
+            },
+            {
+              key: "distribution2",
+              value: 20,
+            },
+            {
+              key: "distribution3",
+              value: 20,
+            },
+            {
+              key: "distribution4",
+              value: 20,
+            },
+            {
+              key: "distribution5",
+              value: 20,
+            },
+            {
+              key: "distribution6",
+              value: 20,
+            },
+            {
+              key: "distribution7",
+              value: 20,
+            },
+            {
+              key: "distribution8",
+              value: 20,
+            },
+            {
+              key: "distribution9",
+              value: 20,
+            },
+            {
+              key: "distribution10",
+              value: 20,
+            },
+            {
+              key: "distribution11",
+              value: 20,
+            },
+          ],
+        },
+      ],
+    });
+
+    expect(screen.getByText("distribution9")).toBeInTheDocument();
+    expect(screen.getByText("other")).toBeInTheDocument();
   });
 });
